0faa3856bd0c647b806120b4748647472edcde04
Hamza El Aidi <helaidi@exomind.fr>
2022-10-17 20:19:00 +0200

    Add weather icones + using Images enum

--- EPS-Weather/Modules/Cities/CitiesViewController.swift
+++ EPS-Weather/Modules/Cities/CitiesViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class CitiesViewController: UIViewController {
+	
+	@IBOutlet private weak var img: UIImageView!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -17,6 +19,7 @@ class CitiesViewController: UIViewController {
 			viewModel.getWeather(params: Coordinate(lat: 48.11220330007356, lon: -1.6822433953343767)) { result in
 				switch result {
 				case .success(let weather):
+					self.img.image = Images(rawValue: weather.weather.first!.icon)?.image
 					print(weather)
 				case .failure(let error):
 					print(error)
