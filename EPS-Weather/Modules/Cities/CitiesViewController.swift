2560b00e10fa7b32c28331b3cd147ac9637f863e
Hamza El Aidi <helaidi@exomind.fr>
2022-10-17 15:52:33 +0200

    Separate files using MVVM architecture + remove SceneDelegate

--- EPS-Weather/Modules/Cities/CitiesViewController.swift
+++ EPS-Weather/Modules/Cities/CitiesViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CitiesViewController.swift
 //  EPS-Weather
 //
 //  Created by Hamza EL Aidi on 14/10/2022.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CitiesViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
